4157e5ab35154de247e380d337a9541cb5b48eae
Praveen777green <pkumarpkumar464@gmail.com>
2024-07-23 18:43:18 +0530

    file update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='Praveen Kumar',
+    author_email='praveen.kumar@tcs.com',
+    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    packages=find_packages()
+    )
